fab5ce9c34ab63923d1ef8cb2b94771fa40f6a43
Lalo8 <wanang.elodie@hotmail.fr>
2017-04-01 11:14:58 +0200

    update 1
    config.rb + index.html updated

--- config.rb
+++ config.rb
@@ -24,7 +24,7 @@
 #   page "/admin/*"
 # end
 
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# Proxy pages (https://middlemanapp.com/advanced/dynamic_pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
 
@@ -53,6 +53,14 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
+helpers do 
+  def find_author(author)
+   result = data.authors.select {|a| a.keys.first == author}
+   raise ArgumentError unless result.any?
+   result.first
+  end
+end
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
@@ -65,14 +73,13 @@ configure :build do
   # activate :asset_hash
 
   # Use relative URLs
-  activate :relative_assets
-  set :relative_links, true
+  # activate :relative_assets
+    set :relative_links, true
 
   # Or use a different image path
+
+
   # set :http_prefix, "/Content/images/"
-  #
-  require_relative "./lib/build_cleaner"
-  activate :build_cleaner
 end
 
 # Deployment
@@ -86,3 +93,39 @@ activate :deploy do |deploy|
   # deploy.strategy = :submodule # commit strategy: can be :force_push or :submodule, default: :force_push
   # deploy.commit_message = 'custom-message' # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
 end
+
+activate :blog do |blog|
+  blog.layout = "article_layout"
+  blog.tag_template = "tag.html"
+  blog.calendar_template = "calendar.html"
+  blog.prefix = "bl"
+  blog.default_extension = ".md"
+  blog.sources = "posts/{year}/{month}/{day}-{title}.html"
+  blog.permalink = "posts/{year}/{month}/{day}-{title}.html"
+
+end
+
+configure :development do
+  activate :disqus do |d|
+    # using a special shortname
+    d.shortname = "lalo"
+  end
+end
+
+configure :build do
+  activate :disqus do |d|
+    # using a different shortname for production builds
+    d.shortname = "elodiewanang"
+  end
+
+def find_author(author_slug)
+  author_slug = author_slug.downcase
+  result = data.authors.select {|author| author.keys.first == author_slug }
+  raise ArgumentError unless result.any?
+  result.first
+end
+
+end
+
+###image optimizations
+activate :minify_html
