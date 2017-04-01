b1389ddf850ac81e5681cb88f9d4e82befec6f23
Lalo8 <wanang.elodie@hotmail.fr>
2017-04-01 11:22:54 +0200

    update 3
    javascript updated

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,3 +1,38 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+<!-- navbarscroll -->
+
+$(document).ready(function() {
+  // navbar transition jQuery script
+  $(window).scroll(function(e){
+    if ($(this).scrollTop() > 60) {
+      $(".navbar").css({
+        "background": "rgb(255,200,54)",
+        "box-shadow": "0 0 2px"
+      });
+    }
+    else {
+      $(".navbar").css({
+        "background": "transparent",
+        "box-shadow": "0 0 0px transparent"
+      });
+    }
+  });
+});
+
+<!-- smoothscroll -->
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
